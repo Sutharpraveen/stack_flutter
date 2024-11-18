ab8ed1a5377e2236489124195de7c6ae905c0e2c
Praveen Suthar <Sutharpraveen234@gmail.com>
2024-11-19 00:57:59 +0530

    new code

---
+++ lib/screen/ram.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class Ram extends StatefulWidget {
+  const Ram({super.key});
+
+  @override
+  State<Ram> createState() => _RamState();
+}
+
+class _RamState extends State<Ram> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+
+
+    );
+  }
+}
