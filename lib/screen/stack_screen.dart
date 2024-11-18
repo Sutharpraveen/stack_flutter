f19fbb23b7dd2f37091cf6e6a9429b84e6b713b4
Praveen Suthar <Sutharpraveen234@gmail.com>
2024-11-19 00:17:37 +0530

    newdsfsdfds

--- lib/screen/stack_screen.dart
+++ lib/screen/stack_screen.dart
@@ -8,7 +8,7 @@ class FlutterStackExamples extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Stack Examples',
       home: Scaffold(
-        appBar: AppBar(title: Text('Flutter Stack Examples praveen')),
+        appBar: AppBar(title: Text('Flutter Stack')),
         body: ExamplesList(),
       ),
     );
