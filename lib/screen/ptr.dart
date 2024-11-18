87f403b0c8a89d7cc97d4be988910c5052049f9f
Praveen Suthar <Sutharpraveen234@gmail.com>
2024-11-19 00:55:51 +0530

    dsf

---
+++ lib/screen/ptr.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Ssdfd extends StatefulWidget {
+  const Ssdfd({super.key});
+
+  @override
+  State<Ssdfd> createState() => _SsdfdState();
+}
+
+class _SsdfdState extends State<Ssdfd> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
